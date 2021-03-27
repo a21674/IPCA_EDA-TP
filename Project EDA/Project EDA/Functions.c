b3a28feb816778635e91e169480d9b03bb148d3c
Oscar Araujo <73696040+os271989@users.noreply.github.com>
2021-03-27 15:09:47 +0000

    Add files via upload
    codigo

---
+++ Project EDA/Project EDA/Functions.c
@@ -0,0 +1,9 @@
+/*****************************************************************//**
+ * \file   Functions.c
+ * \brief  Ficheiro que contem todas as funçoes e procedimentos a ser utilizados na funçao main
+ * \email  
+ * \author 20844_Oscar Araujo / 20845_Elson Simoes / 21674_Rui Lopes
+ * \date   24 March 2021
+ *********************************************************************/
+
+#include "Files.h"
---
+++ Project EDA/Project EDA/Main.c
@@ -0,0 +1,20 @@
+/*****************************************************************//**
+ * \file   Main.c
+ * \brief  Funçao main que contem todas as iteraçoes e chamadas de funçoes
+ * \email  
+ * \author 20844_Oscar Araujo / 20845_Elson Simoes / 21674_Rui Lopes
+ * \date   24 March 2021
+ *********************************************************************/
+
+#include "Files.h"
+
+void main()
+{
+
+
+
+
+
+
+
+}
