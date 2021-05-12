c1fb3ce5ffec4d6efbfa3fa0c72a7c69a2780dba
Oscar Araujo <73696040+os271989@users.noreply.github.com>
2021-05-12 10:43:47 +0100

    Delete Project EDA directory

--- Project EDA/Project EDA/Functions.c
+++
@@ -1,9 +0,0 @@
-/*****************************************************************//**
- * \file   Functions.c
- * \brief  Ficheiro que contem todas as funçoes e procedimentos a ser utilizados na funçao main
- * \email  
- * \author 20844_Oscar Araujo / 20845_Elson Simoes / 21674_Rui Lopes
- * \date   24 March 2021
- *********************************************************************/
-
-#include "Files.h"
